1da2c53728ea2840220224f9832a9c751c31203a
ollieh-m <ollie.hm@cantab.net>
2016-08-05 18:10:15 +0100

    Initial Commit

---
+++ GiraffeTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiraffeTable
+//
+//  Created by Ollie Haydon-Mulligan on 05/08/2016.
+//  Copyright © 2016 Ollie Haydon-Mulligan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
